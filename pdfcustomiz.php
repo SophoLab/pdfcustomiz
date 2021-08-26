41be52f3512a8a38d582cd2f299f2c057b421d0d
Samuel Bouye <bouyesamuel@gmail.com>
2021-08-26 10:13:14 +0000

    display customiz text on pdf

--- pdfcustomiz.php
+++ pdfcustomiz.php
@@ -1,4 +1,29 @@
 <?php
+/*
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2016 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 
 class Pdfcustomiz extends Module
 {
@@ -46,12 +71,38 @@ class Pdfcustomiz extends Module
 
     public function install()
     {
-        return parent::install();
+        return parent::install()
+            && $this->registerHook('actionPDFInvoiceRender')
+            && $this->registerHook('displayPDFDeliverySlip')
+            && $this->registerHook('displayPDFDeliverySlip')
+            && $this->registerHook('displayPDFInvoice');
     }
 
 
     public function uninstall()
     {
         return parent::uninstall();
+    }
+
+    // public function HookActionPDFInvoiceRender($params)
+    // {
+    //     if ($module = Module::getInstanceByName('citylist')) {
+    //         dump($params);
+    //         dump($module);
+    //         die();
+    //     }
+    // }
+
+    public function HookDisplayPDFDeliverySlip($params)
+    {
+        return $this->HookDisplayPDFInvoice($params);
+    }
+
+    public function HookDisplayPDFInvoice($params)
+    {
+        // dump($params['object']->id_od);
+
+        // die();
+        return $this->display(__FILE__, 'views/templates/hook/invoice.tpl');
     }
 }
---
+++ views/templates/hook/invoice.tpl
@@ -0,0 +1,28 @@
+<div class="panel everheader">
+    <div class="panel-body">
+        <div class="col-md-12">
+            <div class="table-responsive">
+                <table>
+                    <tr class="center small bold center">
+                        <td>{l s='customiz informations' mod='pdfcustomiz'}</td>
+                    </tr>
+                </table>
+                <table id="everpsshippingperpostcode"
+                    class="display responsive nowrap dataTable no-footer dtr-inline collapsed table">
+                    <thead>
+                        <tr class="center small grey bold center">
+                            <th>{l s='Custom title' mod='pdfcustomiz'}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="option_value center small">
+                                This is a customiz pdf module
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
